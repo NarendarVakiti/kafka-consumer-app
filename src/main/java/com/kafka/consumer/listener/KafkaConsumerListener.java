da850a44ea3ed2ad6957d0401bc17bb0251d7bd2
Narendar Vakiti <nvakiti@nisum.com>
2020-03-26 19:01:40 +0530

    added loggers

--- src/main/java/com/kafka/consumer/listener/KafkaConsumerListener.java
+++ src/main/java/com/kafka/consumer/listener/KafkaConsumerListener.java
@@ -1,19 +1,22 @@
 package com.kafka.consumer.listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import com.kafka.consumer.model.User;
 
 @Service
 public class KafkaConsumerListener {
+	private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerListener.class);
 
     @KafkaListener(topics = "KAFKA-TOPIC", groupId = "group_id")
     public void consume(String message){
-        System.out.println("Consumed Message :: "+ message);
+        logger.info("Consumed Message :: "+message);
     }
 
     @KafkaListener(topics = "KAFKA-TOPIC-JSON", groupId = "group_id_json", containerFactory = "userKafkaListenerContainerFactory")
     public void consumeJson(User user){
-        System.out.println("Consumed Message :: "+ user);
+    	logger.info("Consumed Message :: "+ user);
     }
 }
